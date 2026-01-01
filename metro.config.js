56d1d9ca544bc23c2dbef6b375225d12b8be9ace
Sami Horttanainen <sami1horttanainen@gmail.com>
2026-01-01 18:28:27 +0200

    eas conf updates

--- metro.config.js
+++ metro.config.js
@@ -1,21 +1,10 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
-// Add the shared_types directory to watchFolders so Metro can follow the symlink
-const sharedTypesPath = path.resolve(__dirname, '../shared_types');
-config.watchFolders = [sharedTypesPath];
-
-// Configure resolver to handle the symlinked package
-config.resolver.nodeModulesPaths = [
-  path.resolve(__dirname, 'node_modules'),
-  path.resolve(__dirname, '../shared_types/node_modules'),
-];
-
-// Ensure Metro resolves modules from both the app and shared_types
+// Configure path aliases for Metro bundler
 config.resolver.extraNodeModules = {
-  '@groceries/shared_types': sharedTypesPath,
+  '@': __dirname,
 };
 
 module.exports = config;
